9ee81fbc4b3d2984d3d399cb0f970e85da631746
Prashani Bhagya <bhagyamap.23@uom.lk>
2026-04-11 19:40:53 +0530

    feat: backend-login

--- backend/trackngo-backend/auth-user-module/src/main/java/com/trackngo/auth/api/dto/UserDto.java
+++ backend/trackngo-backend/auth-user-module/src/main/java/com/trackngo/auth/api/dto/UserDto.java
@@ -1,15 +1,18 @@
-
 package com.trackngo.auth.api.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
     private Long id;
     @NotBlank
     private String username;
+    @NotBlank
     private String password;
     private String role;
 }
-

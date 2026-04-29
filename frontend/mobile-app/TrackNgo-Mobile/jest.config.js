571133f6d174aac92b78f436e0ed9a9dfddb6f81
dinithrathnayaka23 <rathnayakarmdno.23@uom.lk>
2026-04-29 15:06:43 +0530

    test configurations fixed
    jest test configurations fixed

--- frontend/mobile-app/TrackNgo-Mobile/jest.config.js
+++ frontend/mobile-app/TrackNgo-Mobile/jest.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   preset: 'jest-expo',
   testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testMatch: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   transformIgnorePatterns: [
